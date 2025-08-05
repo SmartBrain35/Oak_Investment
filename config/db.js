49785d242e7d15013e1f0307ef7c6e1f84bee4dc
SmartBrain35 <smartgirl8096@gmail.com>
2025-08-05 07:04:25 -0700

    server improved

--- config/db.js
+++ config/db.js
@@ -5,15 +5,13 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    // Attempt to connect to MongoDB using the URI from environment variables
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB Connected...'); // Log success message
+       });
+
+    console.log('MongoDB Connected...');
   } catch (err) {
-    console.error(`MongoDB Connection Error: ${err.message}`); // Log error message
-    process.exit(1);
+    console.error(`MongoDB Connection Error: ${err.message}`);
+    process.exit(1); 
   }
 };
 
